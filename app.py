f29735c45ce0dc6a077d471ac0050a3080208637
a-khomitskyi <homitskiy.andriy@gmail.com>
2021-10-19 12:45:06 +0300

    third commit

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, jsonify, request, abort, Response
+import db
+
+
+app = Flask(__name__)
+
+
+@app.route('/api/', methods=['GET'])
+def get_collection():
+    dbname = db.get_database()
+    collection_name = dbname["data"]
+    return jsonify(str([x for x in collection_name.find()]))
+
+
+@app.route('/api/', methods=['POST'])
+def insert_entity():
+    json = request.get_json()
+    if not json:
+        abort(400)
+    else:
+        dbname = db.get_database()
+        collection_name = dbname["data"]
+        try:
+            collection_name.insert_one(json)
+        except Exception as _ex:
+            return abort(500)
+    return Response(status=200)
+
+
+@app.route('/api/', methods=['DELETE'])
+def del_item():
+    json = request.get_json()
+    if not json:
+        abort(400)
+    else:
+        dbname = db.get_database()
+        collection_name = dbname["data"]
+        result = collection_name.find_one_and_delete(json)
+        if result is not None:
+            return Response(status=200)
+    return Response(status=501)
+
+
+if __name__ == '__main__':
+    db.initialize()
+    app.run(debug=True)
--- db.py
+++ db.py
@@ -26,41 +26,11 @@ def date_correct(items: dict) -> dict:
     return items
 
 
-def drop_element(entity: dict) -> bool:
-    try:
-        assert entity  # Not empty
-        if len(entity) > 1:
-            collection_name.delete_many(entity)
-        else:
-            collection_name.delete_one(entity)
-        return True
-    except Exception as _ex:
-        print(_ex)
-        return False
-
-
-def collect_elements() -> list:
-    collection = [x for x in collection_name.find()]
-    return collection
-
-
-def insert_entity(entity: dict) -> bool:
-    try:
-        assert entity  # Entity not empty
-        if len(entity) > 1:
-            collection_name.insert_many(entity)
-        else:
-            collection_name.delete_one(entity)
-        return True
-    except Exception as _ex:
-        print(_ex)
-        return False
-
-
-def main():
-    # dbname = get_database()
+def initialize():
+    # Get the DB
+    dbname = get_database()
     # Get the collection from DB
-    # collection_name = dbname["data"]
+    collection_name = dbname["data"]
     # Correcting dates
     data = date_correct(script.main())
     # Pushing changes to DB
@@ -68,6 +38,4 @@ def main():
 
 
 if __name__ == "__main__":
-    dbname = get_database()
-    collection_name = dbname["data"]
-    main()
+    initialize()
